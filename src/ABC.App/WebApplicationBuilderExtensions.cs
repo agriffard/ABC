0e6fbab0054626639fbeaea340af7170f92e34e8
Antoine Griffard <agriffard@hotmail.com>
2023-10-10 16:19:42 +0200

    Extensions

---
+++ src/ABC.App/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,21 @@
+namespace Microsoft.AspNetCore.Builder;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static Microsoft.Extensions.Logging.ILogger AddSerilog(this WebApplicationBuilder builder)
+    {
+        // build logger configuration
+        var loggerConfiguration = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration);
+
+        // create logger instance
+        var logger = loggerConfiguration.CreateLogger();
+
+        // Register the logger
+        builder.Host.UseSerilog(logger);
+
+        // Init static logger factory
+        StaticLoggerConfiguration.SetStaticLoggerFactory((LoggerFactory)new LoggerFactory().AddSerilog(logger));
+
+        return StaticLoggerConfiguration.CreateLogger<Program>();
+    }
+}
--- src/ABC.Core/_Imports.cs
+++ src/ABC.Core/_Imports.cs
@@ -1,3 +1,6 @@
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;
+global using ABC.Core.Configuration;
+global using ABC.Core.Constants;
 global using ABC.Core.Enums;
 global using ABC.Core.Models.Reporting;
